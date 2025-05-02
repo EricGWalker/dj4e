f17b77746ac80b628fa07a945225d8d6164a6456
EricGWalker <ewalkerpriority@gmail.com>
2025-05-02 19:43:56 -0400

    code was stolen from sample, not mine

--- django_projects/mysite/autos/urls.py
+++ django_projects/mysite/autos/urls.py
@@ -1,16 +1,18 @@
 from django.urls import path
-
 from . import views
+from django.views.generic import TemplateView
 
+# https://docs.djangoproject.com/en/4.2/topics/http/urls/
 app_name = "autos"
 urlpatterns = [
-    path("", views.IndexView.as_view(), name="index"),
-    path("make/list", views.Make_List.as_view(), name="make_list"),
-    path("make/create", views.Make_Create.as_view(), name="make_create"),
-    path("make/update", views.Make_Update.as_view(), name="make_update"),
-    path("make/delete", views.Make_Delete.as_view(), name="make_delete"),
-    path("autos/list", views.Autos_List.as_view(), name="autos_list"),
-    path("autos/create", views.Autos_Create.as_view(), name="autos_create"),
-    path("autos/update", views.Autos_Update.as_view(), name="autos_update"),
-    path("autos/delete", views.Autos_Delete.as_view(), name="autos_delete"),
+    path("", views.MainView.as_view(), name="all"),
+    path("main/create/", views.AutoCreate.as_view(), name="auto_create"),
+    path("main/<int:pk>/update/", views.AutoUpdate.as_view(), name="auto_update"),
+    path("main/<int:pk>/delete/", views.AutoDelete.as_view(), name="auto_delete"),
+    path("lookup/", views.MakeView.as_view(), name="make_list"),
+    path("lookup/create/", views.MakeCreate.as_view(), name="make_create"),
+    path("lookup/<int:pk>/update/", views.MakeUpdate.as_view(), name="make_update"),
+    path("lookup/<int:pk>/delete/", views.MakeDelete.as_view(), name="make_delete"),
 ]
+
+# Note that make_ and auto_ give us uniqueness within this application
